12cc6eddcc677a41ef029d90d926308d48e2da78
Chris Allen <cehallen@gmail.com>
2014-03-01 13:11:39 -0500

    Finish build_teams hash.  Return hash, key for each team, and empty w/l columns

--- server.rb
+++ server.rb
@@ -1,32 +1,8 @@
-require 'pry'
-require 'shotgun'
-require 'csv'
+# require 'pry'
+# require 'shotgun'
+# require 'csv'
 # require 'sinatra'
 
-
-
-
-get '/' do
-  erb :index
-end
-
-get '/leaderboard'
-  @league_stats_array = league_stats_array
-  erb :leaderboard
-end
-         ###############
-def build_teams(stats)
-  teams_hash = {}
-  stats.each do |game|
-    if !teams_hash[game[:home_team]].is_a?(String)
-      teams_hash[game[:home_team]] = {w: 0, l: 0}
-    end
-    if
-      !teams_hash[game[:away_team]].is_a?(String)
-      teams_hash[game[:away_team]] = {w: 0, l: 0}
-    end
-  end
-end
          ###############
 stats = [
   {
@@ -54,6 +30,40 @@ stats = [
     away_score: 21
   }
 ]
+         ##############
+# get '/' do
+#   erb :index
+# end
+
+# get '/leaderboard'
+#   @league_stats_array = league_stats_array
+#   erb :leaderboard
+# end
+
+
+         ############### this will create a template of teams with 0 wins and losses
+def build_teams(stats)
+  teams_hash = {}
+  stats.each do |game|
+    if !teams_hash[game[:home_team]].is_a?(Hash)
+      teams_hash[game[:home_team]] = {name: game[:home_team], w: 0, l: 0}
+    end
+    if
+      !teams_hash[game[:away_team]].is_a?(Hash)
+      teams_hash[game[:away_team]] = {name: game[:home_team], w: 0, l: 0}
+    end
+  end
+  teams_hash
+end
+         ############### this will add wins and losses to teams_hash
+def add_wins_losses(teams_hash, stats)
+
+end
+
          ###############
-set :views, File.dirname(__FILE__) + '/views'
-set :public_folder, File.dirname(__FILE__) + '/public'
+# teams_hash = build_teams(stats)
+
+
+         ###############
+# set :views, File.dirname(__FILE__) + '/views'
+# set :public_folder, File.dirname(__FILE__) + '/public'
